b129bf107d89bfea1e9c89a9ebb7b55bba9459e3
pedroksl <pedrosathler@hotmail.com>
2024-08-28 20:11:07 -0300

    Updated quantum computer texture

--- src/main/java/net/pedroksl/advanced_ae/client/renderer/AAECraftingUnitModelProvider.java
+++ src/main/java/net/pedroksl/advanced_ae/client/renderer/AAECraftingUnitModelProvider.java
@@ -15,7 +15,6 @@ import net.pedroksl.advanced_ae.AdvancedAE;
 import net.pedroksl.advanced_ae.common.blocks.AAECraftingUnitType;
 
 import appeng.client.render.crafting.AbstractCraftingUnitModelProvider;
-import appeng.client.render.crafting.UnitBakedModel;
 
 @OnlyIn(Dist.CLIENT)
 public class AAECraftingUnitModelProvider extends AbstractCraftingUnitModelProvider<AAECraftingUnitType> {
@@ -25,6 +24,7 @@ public class AAECraftingUnitModelProvider extends AbstractCraftingUnitModelProvi
     protected static final Material RING_SIDE_HOR = texture("ring_side_hor");
     protected static final Material RING_SIDE_VER = texture("ring_side_ver");
     protected static final Material STRUCTURE_BASE = texture("quantum_structure_formed");
+    protected static final Material STRUCTURE_ANIMATION = texture("quantum_structure_powered");
 
     public AAECraftingUnitModelProvider(AAECraftingUnitType type) {
         super(type);
@@ -41,7 +41,12 @@ public class AAECraftingUnitModelProvider extends AbstractCraftingUnitModelProvi
         TextureAtlasSprite ringSideHor = spriteGetter.apply(RING_SIDE_HOR);
         TextureAtlasSprite ringSideVer = spriteGetter.apply(RING_SIDE_VER);
 
-        return new UnitBakedModel(ringCorner, ringSideHor, ringSideVer, spriteGetter.apply(STRUCTURE_BASE));
+        return new AnimatedCraftingCubeBakedModel(
+                ringCorner,
+                ringSideHor,
+                ringSideVer,
+                spriteGetter.apply(STRUCTURE_BASE),
+                spriteGetter.apply(STRUCTURE_ANIMATION));
     }
 
     private static Material texture(String name) {
---
+++ src/main/java/net/pedroksl/advanced_ae/client/renderer/AnimatedCraftingCubeBakedModel.java
@@ -0,0 +1,54 @@
+package net.pedroksl.advanced_ae.client.renderer;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import net.pedroksl.advanced_ae.common.blocks.AAEAbstractCraftingUnitBlock;
+
+import appeng.client.render.cablebus.CubeBuilder;
+
+public class AnimatedCraftingCubeBakedModel extends AAECraftingCubeBakedModel {
+
+    private final TextureAtlasSprite baseTexture;
+
+    private final TextureAtlasSprite animationTexture;
+
+    public AnimatedCraftingCubeBakedModel(
+            TextureAtlasSprite ringCorner,
+            TextureAtlasSprite ringHor,
+            TextureAtlasSprite ringVer,
+            TextureAtlasSprite baseTexture,
+            TextureAtlasSprite animationTexture) {
+        super(ringCorner, ringHor, ringVer);
+
+        this.baseTexture = baseTexture;
+        this.animationTexture = animationTexture;
+    }
+
+    @Override
+    protected void addInnerCube(
+            Direction facing,
+            BlockState state,
+            ModelData modelData,
+            CubeBuilder builder,
+            float x1,
+            float y1,
+            float z1,
+            float x2,
+            float y2,
+            float z2) {
+        boolean powered = state.getValue(AAEAbstractCraftingUnitBlock.POWERED);
+
+        builder.setTexture(this.baseTexture);
+        builder.addCube(x1, y1, z1, x2, y2, z2);
+
+        if (powered) {
+            builder.setEmissiveMaterial(true);
+            builder.setTexture(this.animationTexture);
+            builder.addCube(x1, y1, z1, x2, y2, z2);
+            // Reset back to default
+            builder.setEmissiveMaterial(false);
+        }
+    }
+}
