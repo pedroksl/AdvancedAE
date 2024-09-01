4984538529e01b13f763d67182b904bfab7018e0
pedroksl <pedrosathler@hotmail.com>
2024-09-01 15:28:46 -0300

    Fixed a client crash that happened when breaking a quantum computer that is working on a craft

--- src/main/java/net/pedroksl/advanced_ae/client/AAESemantics.java
+++ src/main/java/net/pedroksl/advanced_ae/client/AAESemantics.java
@@ -1,20 +1,3 @@
 package net.pedroksl.advanced_ae.client;
 
-import appeng.menu.SlotSemantic;
-import appeng.menu.SlotSemantics;
-
-public class AAESemantics {
-
-    public static final SlotSemantic OUTPUT_SLOT = SlotSemantics.register("OutputSlot", false);
-    public static final SlotSemantic EX_2 = SlotSemantics.register("EX_2", false);
-    public static final SlotSemantic EX_3 = SlotSemantics.register("EX_3", false);
-    public static final SlotSemantic EX_4 = SlotSemantics.register("EX_4", false);
-    public static final SlotSemantic EX_5 = SlotSemantics.register("EX_5", false);
-    public static final SlotSemantic EX_6 = SlotSemantics.register("EX_6", false);
-    public static final SlotSemantic EX_7 = SlotSemantics.register("EX_7", false);
-    public static final SlotSemantic EX_8 = SlotSemantics.register("EX_8", false);
-    public static final SlotSemantic EX_9 = SlotSemantics.register("EX_9", false);
-    public static final SlotSemantic EX_10 = SlotSemantics.register("EX_10", false);
-    public static final SlotSemantic EX_11 = SlotSemantics.register("EX_11", false);
-    public static final SlotSemantic EX_12 = SlotSemantics.register("EX_12", false);
-}
+public class AAESemantics {}
