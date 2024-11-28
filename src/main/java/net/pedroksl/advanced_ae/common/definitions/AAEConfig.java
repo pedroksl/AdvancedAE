0ec64abf94caa239952a14d716073aa30d3f408e
pedroksl <pedrosathler@hotmail.com>
2024-11-28 03:01:54 -0300

    Changed Quantum Computer default max size to 7

--- src/main/java/net/pedroksl/advanced_ae/common/definitions/AAEConfig.java
+++ src/main/java/net/pedroksl/advanced_ae/common/definitions/AAEConfig.java
@@ -160,7 +160,7 @@ public class AAEConfig {
             quantumComputerMaxSize = define(
                     builder,
                     "quantumComputerMaxSize",
-                    5,
+                    7,
                     5,
                     12,
                     "Define the maximum dimensions of the Quantum Computer Multiblock.");
