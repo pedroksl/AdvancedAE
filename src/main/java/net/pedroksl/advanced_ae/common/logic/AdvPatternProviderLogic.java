c9113b5a0ad193c0e6d371e5fc8b1d3778d6fcd6
pedroksl <pedrosathler@hotmail.com>
2024-12-03 10:12:11 -0300

    Updated japanese translations.

--- src/main/java/net/pedroksl/advanced_ae/common/logic/AdvPatternProviderLogic.java
+++ src/main/java/net/pedroksl/advanced_ae/common/logic/AdvPatternProviderLogic.java
@@ -405,7 +405,7 @@ public class AdvPatternProviderLogic implements InternalInventoryHost, ICrafting
             var target = adapterMap.get(what);
             var inserted = 0L;
             if (target != null) {
-                inserted = adapterMap.get(what).insert(what, amount, Actionable.MODULATE);
+                inserted = target.insert(what, amount, Actionable.MODULATE);
             }
             if (inserted < amount) {
                 this.addToSendList(what, amount - inserted);
