627f792eeb6935ac3a2eba9e35890345a130f095
pedroksl <pedrosathler@hotmail.com>
2025-01-23 17:17:51 -0300

    Added the fuzzy card upgrade to the import export bus

--- src/main/java/net/pedroksl/advanced_ae/AdvancedAE.java
+++ src/main/java/net/pedroksl/advanced_ae/AdvancedAE.java
@@ -117,6 +117,7 @@ public class AdvancedAE {
             Upgrades.add(AEItems.CAPACITY_CARD, AAEItems.IMPORT_EXPORT_BUS, 5);
             Upgrades.add(AEItems.REDSTONE_CARD, AAEItems.IMPORT_EXPORT_BUS, 1);
             Upgrades.add(AEItems.CRAFTING_CARD, AAEItems.IMPORT_EXPORT_BUS, 1);
+            Upgrades.add(AEItems.FUZZY_CARD, AAEItems.IMPORT_EXPORT_BUS, 1);
 
             if (Addons.APPFLUX.isLoaded()) {
                 AppliedFluxPlugin.init();
