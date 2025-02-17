c2c8bc3e7384b13c8d0a175694ebb7bb28e3227f
pedroksl <pedrosathler@hotmail.com>
2025-02-17 20:24:57 -0300

    Prevent a crash that could happen when handling events

--- src/main/java/net/pedroksl/advanced_ae/events/AAEPlayerEvents.java
+++ src/main/java/net/pedroksl/advanced_ae/events/AAEPlayerEvents.java
@@ -172,20 +172,22 @@ public class AAEPlayerEvents {
             var nv = serverPlayer.getEffect(MobEffects.NIGHT_VISION);
 
             ItemStack stack = player.getItemBySlot(EquipmentSlot.HEAD);
-            if (stack.getItem() instanceof QuantumHelmet helmet) {
-                if (helmet.isUpgradeEnabledAndPowered(stack, UpgradeType.NIGHT_VISION, serverPlayer.level())) {
-                    if (nv == null || nv.getDuration() < 210) {
-                        stack.set(AAEComponents.NIGHT_VISION_ACTIVATED, true);
-                        serverPlayer.addEffect(
-                                new MobEffectInstance(MobEffects.NIGHT_VISION, 210, 0, false, false, false));
-                        helmet.consumeEnergy(player, stack, UpgradeType.NIGHT_VISION);
+            if (!stack.isEmpty()) {
+                if (stack.getItem() instanceof QuantumHelmet helmet) {
+                    if (helmet.isUpgradeEnabledAndPowered(stack, UpgradeType.NIGHT_VISION, serverPlayer.level())) {
+                        if (nv == null || nv.getDuration() < 210) {
+                            stack.set(AAEComponents.NIGHT_VISION_ACTIVATED, true);
+                            serverPlayer.addEffect(
+                                    new MobEffectInstance(MobEffects.NIGHT_VISION, 210, 0, false, false, false));
+                            helmet.consumeEnergy(player, stack, UpgradeType.NIGHT_VISION);
+                        }
                     }
                 }
-            }
-            if (stack.getOrDefault(AAEComponents.NIGHT_VISION_ACTIVATED, false)) {
-                if (nv != null && nv.getDuration() < 210) {
-                    serverPlayer.removeEffect(MobEffects.NIGHT_VISION);
-                    stack.remove(AAEComponents.NIGHT_VISION_ACTIVATED);
+                if (stack.getOrDefault(AAEComponents.NIGHT_VISION_ACTIVATED, false)) {
+                    if (nv != null && nv.getDuration() < 210) {
+                        serverPlayer.removeEffect(MobEffects.NIGHT_VISION);
+                        stack.remove(AAEComponents.NIGHT_VISION_ACTIVATED);
+                    }
                 }
             }
         } else {
