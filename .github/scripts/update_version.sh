910009a01e6ac859756f48ee218343b7a6dcaaec
Pedro Sathler <pedrosathler@hotmail.com>
2025-11-04 03:39:17 -0300

    Updated publishing scripts

--- .github/scripts/update_version.sh
+++ .github/scripts/update_version.sh
@@ -5,15 +5,21 @@ read -ra v <<< "$version"
 case $UPDATE_TYPE in
   breaking)
     ((v[0]++))
+    v[1]=0
+    v[2]=0
     ;;
   major)
     ((v[1]++))
+    v[2]=0
     ;;
   minor)
     ((v[2]++))
+    ;;
 esac
 
 new_version=${v[0]}.${v[1]}.${v[2]}
+
+echo $new_version
 
 sed -i -E "s/(mod_version=).*(-1.21.1)/\1$new_version\2/" ./gradle.properties
 sed -i -E "s/(version_type=).*/\1$RELEASE_TYPE/" ./gradle.properties
