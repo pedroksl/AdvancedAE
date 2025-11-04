145aa273f27e70996da632db9fad718160e90131
Pedro Sathler <pedrosathler@hotmail.com>
2025-11-04 00:30:32 -0300

    Updated project CI

---
+++ .github/scripts/update_version.sh
@@ -0,0 +1,19 @@
+version=$(echo $VERSION | sed -nE 's/([0-9]+)\.([0-9]+)\.([0-9]+)-1.21.1/\1 \2 \3/p')
+
+read -ra v <<< "$version"
+
+case $UPDATE_TYPE in
+  breaking)
+    ((v[0]++))
+    ;;
+  major)
+    ((v[1]++))
+    ;;
+  minor)
+    ((v[2]++))
+esac
+
+new_version=${v[0]}.${v[1]}.${v[2]}
+
+sed -i -E "s/(mod_version=).*(-1.21.1)/\1$new_version\2/" ./gradle.properties
+sed -i -E "s/(version_type=).*/\1$RELEASE_TYPE/" ./gradle.properties
